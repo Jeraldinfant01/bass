7497aed240c22fbf2afc1a058fe7b9db3429ea56
JeraldInfant01 <jeraldinfant01@gmail.com>
2023-05-18 12:25:04 +0530

    codepushed

---
+++ src/bass/org/bass.java
@@ -0,0 +1,35 @@
+package bass.org;
+
+public class bass {
+
+	public static void main(String[] args) {
+		
+		int c=1000;
+		
+		
+		if (c==1000) {
+			System.out.println("the name is rass");
+		}
+		
+		else if (c==1500) {
+			System.out.println("the name is trass");
+		} else {
+			
+			
+			System.out.println("the name is null");
+			
+			
+			
+
+		}
+		
+		
+	
+		
+		
+	}
+	
+	
+	
+	
+}
